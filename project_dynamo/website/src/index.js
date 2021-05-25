6ae0eea30439a8a602423f0dffc860d765ec64c3
Bastian Hodapp <bastianhodapp@gmail.com>
2021-05-25 15:49:50 +0200

    Add jspdf with example code

--- project_dynamo/website/src/index.js
+++ project_dynamo/website/src/index.js
@@ -1,4 +1,5 @@
 import "./styles/styles.css";
+import { jsPDF } from "jspdf";
 
 //#region main
 document.querySelector("#led-on-button").addEventListener("click", () => {
@@ -8,5 +9,12 @@ document.querySelector("#led-on-button").addEventListener("click", () => {
 document.querySelector("#led-off-button").addEventListener("click", () => {
     console.log("Turning LED OFF");
     fetch("/led/off");
+});
+
+document.querySelector("#pdf-button").addEventListener("click", () => {
+    const doc = new jsPDF();
+
+    doc.text("Hello world!", 10, 10);
+    doc.save("urkunde.pdf");
 });
 //#endregion main
